e3a04847fc3a2f006e61f679edc13d198df0e592
KoharaKazuya <reflect.results@gmail.com>
2012-12-29 17:58:19 +0900

    テスト用 Javascript の変更

--- test/test.js
+++ test/test.js
@@ -1,3 +1,7 @@
 $(function() {
-    new Node($("canvas"));
+    var im = new IDManager();
+    var node1 = new Node($("canvas"), im);
+    var node2 = new Node($("canvas"), im);
+    var engine = new PhysicsEngine();
+    engine.update([node1, node2]);
 });
