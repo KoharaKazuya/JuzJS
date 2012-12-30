e308e43f7ac62d4a534dc8fc99626ff4eee98bad
KoharaKazuya <reflect.results@gmail.com>
2012-12-30 13:58:38 +0900

    引力設定のテストを記述

--- test/test.js
+++ test/test.js
@@ -1,9 +1,15 @@
 module("", {
     setup: function() {
+        this.canvas = $("canvas");
+        this.im = new IDManager();
     },
     teardown: function() {
     }
 });
 test("ノード間の引力", function() {
-    ok(false, "未実装");
+    var node1 = new Node(this.canvas, this.im.getJCanvasUniqueName());
+    var node2 = new Node(this.canvas, this.im.getJCanvasUniqueName());
+    equal(node1.getGravity(node2), 0, "引力の初期値は 0");
+    node1.setGravity(node2, 1);
+    equal(node2.getGravity(node2), 1, "引力を 1 に設定");
 });
